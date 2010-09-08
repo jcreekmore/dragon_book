4fe93ab460642ddae412c31cb499210f424f1e4d
Jonathan Creekmore <jonathan@thecreekmores.org>
2010-09-07 19:32:06 -0500

    Add initial code set for P2.2, plus a description of the problem.

---
+++ Chapter_2/P2.2/global.h
@@ -0,0 +1,31 @@
+#include <stdio.h>      /* load i/o routines */
+#include <ctype.h>      /* load character test routines */
+
+#define BSIZE  128		/* buffer size */
+#define NONE   -1
+#define EOS	   '\0'
+
+#define NUM	   256
+#define DIV	   257
+#define MOD	   258
+#define ID	   259
+#define DONE   260
+
+int tokenval;      /* value of token attribute */
+int lineno;
+
+struct entry {     /* form of symbol table entry */
+	char *lexptr;
+	int token;
+};
+
+struct entry *symtable;
+
+
+extern int lexan(void);
+extern void parse(void);
+extern void emit(int t, int tval);
+extern int lookup(char s[]);
+extern int insert(char s[], int tok);
+extern void init(void);
+extern void error(char *m);
